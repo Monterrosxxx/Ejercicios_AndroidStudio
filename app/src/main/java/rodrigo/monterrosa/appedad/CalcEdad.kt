932a94cd560a27d6b2c5e6418324c854e387a9d4
Rodrigo Monterrosa <20220377@ricaldone.edu.sv>
2024-03-21 12:11:14 -0600

    App edad terminada

---
+++ app/src/main/java/rodrigo/monterrosa/appedad/CalcEdad.kt
@@ -0,0 +1,14 @@
+package rodrigo.monterrosa.appedad
+
+class CalcEdad {
+
+    var ano1:Int = 2024
+    fun restar (ano2:Int):Int{
+
+        var resultado = ano1 - ano2
+
+        return resultado
+
+    }
+
+}
--- app/src/main/java/rodrigo/monterrosa/appedad/MainActivity.kt
+++ app/src/main/java/rodrigo/monterrosa/appedad/MainActivity.kt
@@ -1,12 +1,17 @@
 package rodrigo.monterrosa.appedad
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val txtNacimiento = findViewById<EditText>(R.id.txtNacimiento)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val txtEdad = findViewById<TextView>(R.id.txtEdad)
+
+        val objCalcEdad = CalcEdad()
+
+        btnCalcular.setOnClickListener {
+
+            val resul = objCalcEdad.restar(txtNacimiento.text.toString().toInt())
+            println("Su edas entonces es de $resul jaja tas viejo")
+            txtEdad.text = "El resultado es $resul ni modo, ya te vas a morir xddd"
+
+
         }
     }
 }
